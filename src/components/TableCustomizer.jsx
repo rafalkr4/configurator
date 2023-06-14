7ee8cb1b2b82cd6a42ae75eba6cad36b5c17d1c8
Rafał Krasowski <rrafal.ka@gmail.com>
2023-06-14 15:43:54 +0200

    working

--- src/components/TableCustomizer.jsx
+++ src/components/TableCustomizer.jsx
@@ -1,63 +1,36 @@
-import React, { useState } from 'react';
-import { Slider, Typography, Box, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import React from 'react';
+import { FormControl, InputLabel, MenuItem, Select, Typography, Box, FormControlLabel, Switch } from '@mui/material';
+
+const options = ['short', 'medium', 'tall'];
 
 function TableCustomizer({ dimensions, setDimensions }) {
-  const [errors, setErrors] = useState({});
-
-  const validate = (property, value) => {
-    if (!value) {
-      setErrors(prev => ({
-        ...prev,
-        [property]: 'This field is required'
-      }));
-    } else {
-      setErrors(prev => ({
-        ...prev,
-        [property]: ''
-      }));
-    }
-  };
-
-  const handleChange = (property) => (event) => {
-    const value = event.target.value;
-    validate(property, value);
-    setDimensions({
-      ...dimensions,
-      [property]: value
-    });
-  };
-
-  return (
-    <Box sx={{ my: 2 }}>
-      <Typography variant="h6" gutterBottom>Customize your table</Typography>
-      <Box sx={{ mb: 1 }}>
-        <FormControl fullWidth>
-          <InputLabel>Height</InputLabel>
-          <Select value={dimensions.height} onChange={handleChange('height')}>
-            <MenuItem value=""><em>None</em></MenuItem>
-            <MenuItem value="short">Short</MenuItem>
-            <MenuItem value="medium">Medium</MenuItem>
-            <MenuItem value="tall">Tall</MenuItem>
-          </Select>
-        </FormControl>
-        {errors.height && <Typography color="error">{errors.height}</Typography>}
-      </Box>
-
-      // Similar blocks for Length and Width here
-
-      <Box sx={{ mt: 1 }}>
-        <Typography id="rounded-slider" gutterBottom>Rounded Corners</Typography>
-        <Slider
-          valueLabelDisplay="auto"
-          min={0}
-          max={1}
-          step={1}
-          value={dimensions.rounded ? 1 : 0}
-          onChange={(_, value) => setDimensions({ ...dimensions, rounded: Boolean(value) })}
-        />
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ p: 2 }}>
+            <Typography variant="h6" gutterBottom>Customize your table</Typography>
+            {['length', 'width', 'height'].map((dimension) => (
+                <Box key={dimension} sx={{ mb: 3 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id={`${dimension}-select-label`}>{dimension}</InputLabel>
+                        <Select
+                            labelId={`${dimension}-select-label`}
+                            id={`${dimension}-select`}
+                            value={dimensions[dimension]}
+                            label={dimension}
+                            onChange={(event) => setDimensions({ ...dimensions, [dimension]: event.target.value })}
+                        >
+                            {options.map((option) => (
+                                <MenuItem key={option} value={option}>{option}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+            ))}
+            <FormControlLabel
+                control={<Switch checked={dimensions.rounded} onChange={(event) => setDimensions({ ...dimensions, rounded: event.target.checked })} />}
+                label={<Typography style={{color: dimensions.rounded ? "black" : "grey"}}>Rounded corners</Typography>}
+            />
+        </Box>
+    );
 }
 
 export default TableCustomizer;
--- src/components/TableDisplay.jsx
+++ src/components/TableDisplay.jsx
@@ -1,97 +1,31 @@
-// import * as React, {useState} from "react";
-// import { Button, MenuItem, Typography, Slider, Grid } from "@mui/material-next/Button";
-// import { makeStyles } from '@material-ui/core/styles';
-//
-// const useStyle = makeStyle(theme => ({
-//     root: {
-//         padding: theme.spacing(2),
-//     },
-//     image: {
-//         width: "100%",
-//         height: "auto",
-//     },
-//     select: {
-//         margin: theme.spacing(1),
-//     },
-// }));
-//
-// function tableDisplay() {
-//     const classes = useStyle();
-//     const [height, setHeight] = useState("short");
-//     const [width, setWidth] = useState("short");
-//     const [length, setLength] = useState("short");
-//     const [rounded, setRounded] = useState(0);
-//
-//
-//     const tableImages = {
-//         short: {
-//             short: "",
-//             medium: "",
-//             long: "",
-//         },
-//
-//     medium: {
-//             short: "",
-//         medium: "",
-//         long: "",
-//     },
-//     long: {
-//             short: "",
-//         medium: "",
-//         long: "",
-//     },
-//     };
-//
-// return (
-//     <Grid container direction={{window.innerWidth < 900 ? "column-reverse" : "row"}>
-//         <Grid item xs={12} md={6}>
-//         <img className={class.image} src={tableImages[height][width][length]} alt="Table preview" />
-//     </Grid>
-//         <Grid item xs={12} md={6}>
-//         <Select className={classes.select} value={height} onChange={e => setHeight(e.target.value)}>
-//         <Menu value="short">Krótkie</MenuItem>
-//         <Menu value="medium">Średnie</MenuItem>
-//         <Menu value="long">Długie</MenuItem>
-//         </Select>
-//         <Select className={classes.select} value={width} onChange={e => setWidth(e.target.value)}>
-//
-//
-//
-// )
-//
-//
-//
-// export default TableDisplay;
-
-
 import React from 'react';
 import { Box } from '@mui/material';
 
-import shortTableImage from '../images/shortTableImage.png';
-import mediumTableImage from '../images/mediumTableImage.png';
-import tallTableImage from '../images/tallTableImage.png';
-import roundedTableImage from '../images/roundedTableImage.png';
+// These could be any values you want to represent the different sizes.
+const sizeMap = {
+    short: 50,
+    medium: 100,
+    tall: 150
+};
 
 function TableDisplay({ dimensions }) {
+    const { length, width, height } = dimensions;
+    const divStyle = {
+        height: `${sizeMap[height]}px`,
+        width: `${sizeMap[length]}px`,
+        backgroundColor: 'saddlebrown',
+        borderRadius: dimensions.rounded ? "7%" : "0",
+    };
 
-    let tableImageSrc = '';
-
-    if (dimensions.height === 'short' && dimensions.length === 'short' && dimensions.width === 'short') {
-        tableImageSrc = shortTableImage;
-    } else if (dimensions.height === 'medium' && dimensions.length === 'medium' && dimensions.width === 'medium') {
-        tableImageSrc = mediumTableImage;
-    } else if (dimensions.height === 'tall' && dimensions.length === 'tall' && dimensions.width === 'tall') {
-        tableImageSrc = tallTableImage;
-    } else if (dimensions.rounded) {
-        tableImageSrc = roundedTableImage;
-    }
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
-            <img src={tableImageSrc} alt="Custom table" />
+        <Box sx={{ p: 2 }}>
+            <div style={divStyle} />
+            <Box sx={{ mt: 2 }}>
+                <strong>Dimensions:</strong> Length: {length}, Width: {width}, Height: {height}
+            </Box>
         </Box>
     );
 }
 
 export default TableDisplay;
-
